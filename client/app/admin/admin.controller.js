62574e307f76d8d970a593b9c35229a0eb24aa31
Dane Vanderbilt <danevandy@hotmail.com>
2016-03-07 17:24:02 -0600

    optimized rankings and added admin search

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -24,145 +24,147 @@ class AdminController {
 
   setSearch() {
     this.$http.get('/api/rankings/getrankings').then(response1 => {
-      this.brackets = response1.data;
-      this.$http.get('/api/masterbracket?round=1').then(response2 => {
-        this.round1 = response2.data.toString().split(',');
-        this.$http.get('/api/masterbracket?round=2').then(response3 => {
-          this.round2 = response3.data.toString().split(',');
-          this.$http.get('/api/masterbracket?round=3').then(response4 => {
-            this.round3 = response4.data.toString().split(',');
-            this.$http.get('/api/masterbracket?round=4').then(response5 => {
-              this.round4 = response5.data.toString().split(',');
-              this.$http.get('/api/masterbracket?round=5').then(response6 => {
-                this.round5 = response6.data.toString().split(',');
-                this.$http.get('/api/masterbracket?round=6').then(response7 => {
-                  this.round6 = response7.data.toString().split(',');
-                  for (var a = 0; a < this.brackets.length; a++) {
+      if (this.brackets.length === 0) {
+        this.brackets = response1.data;
+        this.$http.get('/api/masterbracket?round=1').then(response2 => {
+          this.round1 = response2.data.toString().split(',');
+          this.$http.get('/api/masterbracket?round=2').then(response3 => {
+            this.round2 = response3.data.toString().split(',');
+            this.$http.get('/api/masterbracket?round=3').then(response4 => {
+              this.round3 = response4.data.toString().split(',');
+              this.$http.get('/api/masterbracket?round=4').then(response5 => {
+                this.round4 = response5.data.toString().split(',');
+                this.$http.get('/api/masterbracket?round=5').then(response6 => {
+                  this.round5 = response6.data.toString().split(',');
+                  this.$http.get('/api/masterbracket?round=6').then(response7 => {
+                    this.round6 = response7.data.toString().split(',');
+                    for (var a = 0; a < this.brackets.length; a++) {
 
-                    var eliminatedTeams = [];
-                    var i = 0;
+                      var eliminatedTeams = [];
+                      var i = 0;
 
-                    this.brackets[a].round1 = this.brackets[a].bracket[1];
-                    this.brackets[a].round2 = this.brackets[a].bracket[2];
-                    this.brackets[a].round3 = this.brackets[a].bracket[3];
-                    this.brackets[a].round4 = this.brackets[a].bracket[4];
-                    this.brackets[a].round5 = this.brackets[a].bracket[5];
-                    this.brackets[a].round6 = this.brackets[a].bracket[6];
+                      this.brackets[a].round1 = this.brackets[a].bracket[1];
+                      this.brackets[a].round2 = this.brackets[a].bracket[2];
+                      this.brackets[a].round3 = this.brackets[a].bracket[3];
+                      this.brackets[a].round4 = this.brackets[a].bracket[4];
+                      this.brackets[a].round5 = this.brackets[a].bracket[5];
+                      this.brackets[a].round6 = this.brackets[a].bracket[6];
 
-                    for ( i = 0; i < 32; i++) {
-                      var id = this.brackets[a].owner + this.brackets[a].name + i + '32';
-                      var value = document.getElementById(id).value;
-                      if (this.round1[i] == '' && eliminatedTeams.indexOf(value) < 0) {
-                        document.getElementById(id).style.backgroundColor = '#3498db';
-                      } else if (this.round1[i] === value) {
-                        document.getElementById(id).style.backgroundColor = '#2ecc71';
-                        document.getElementById(id).style.borderColor = '#27ae60';
-                      } else {
-                        document.getElementById(id).style.backgroundColor = '#e74c3c';
-                        document.getElementById(id).style.borderColor = '#c0392b';
+                      for ( i = 0; i < 32; i++) {
+                        var id = this.brackets[a].owner + this.brackets[a].name + i + '32';
+                        var value = document.getElementById(id).value;
+                        if (this.round1[i] == '' && eliminatedTeams.indexOf(value) < 0) {
+                          document.getElementById(id).style.backgroundColor = '#3498db';
+                        } else if (this.round1[i] === value) {
+                          document.getElementById(id).style.backgroundColor = '#2ecc71';
+                          document.getElementById(id).style.borderColor = '#27ae60';
+                        } else {
+                          document.getElementById(id).style.backgroundColor = '#e74c3c';
+                          document.getElementById(id).style.borderColor = '#c0392b';
+                        }
+
+                        if (this.round1[i] !== '' && this.round1[i] !== value && eliminatedTeams.indexOf(value) < 0) {
+                          eliminatedTeams.push(value);
+                        }
                       }
 
-                      if (this.round1[i] !== '' && this.round1[i] !== value && eliminatedTeams.indexOf(value) < 0) {
+                      for ( i = 0; i < 16; i++) {
+                        var id = this.brackets[a].owner + this.brackets[a].name + i + '16';
+                        var value = document.getElementById(id).value;
+                        if (this.round2[i] == '' && eliminatedTeams.indexOf(value) < 0) {
+                          document.getElementById(id).style.backgroundColor = '#3498db';
+                        } else if (this.round2[i] === value) {
+                          document.getElementById(id).style.backgroundColor = '#2ecc71';
+                          document.getElementById(id).style.borderColor = '#27ae60';
+                        } else {
+                          document.getElementById(id).style.backgroundColor = '#e74c3c';
+                          document.getElementById(id).style.borderColor = '#c0392b';
+                        }
+
+                        if (this.round2[i] !== '' && this.round2[i] !== value && eliminatedTeams.indexOf(value) < 0) {
                           eliminatedTeams.push(value);
+                        }
+                      }
+
+                      for ( i = 0; i < 8; i++) {
+                        var id = this.brackets[a].owner + this.brackets[a].name + i + '8';
+                        var value = document.getElementById(id).value;
+                        if (this.round3[i] == '' && eliminatedTeams.indexOf(value) < 0) {
+                          document.getElementById(id).style.backgroundColor = '#3498db';
+                        } else if (this.round3[i] === value) {
+                          document.getElementById(id).style.backgroundColor = '#2ecc71';
+                          document.getElementById(id).style.borderColor = '#27ae60';
+                        } else {
+                          document.getElementById(id).style.backgroundColor = '#e74c3c';
+                          document.getElementById(id).style.borderColor = '#c0392b';
+                        }
+
+                        if (this.round3[i] !== '' && this.round3[i] !== value && eliminatedTeams.indexOf(value) < 0) {
+                          eliminatedTeams.push(value);
+                        }
+                      }
+
+                      for ( i = 0; i < 4; i++) {
+                        var id = this.brackets[a].owner + this.brackets[a].name + i + '4';
+                        var value = document.getElementById(id).value;
+                        if (this.round4[i] == '' && eliminatedTeams.indexOf(value) < 0) {
+                          document.getElementById(id).style.backgroundColor = '#3498db';
+                        } else if (this.round4[i] === value) {
+                          document.getElementById(id).style.backgroundColor = '#2ecc71';
+                          document.getElementById(id).style.borderColor = '#27ae60';
+                        } else {
+                          document.getElementById(id).style.backgroundColor = '#e74c3c';
+                          document.getElementById(id).style.borderColor = '#c0392b';
+                        }
+
+                        if (this.round4[i] !== '' && this.round4[i] !== value && eliminatedTeams.indexOf(value) < 0) {
+                          eliminatedTeams.push(value);
+                        }
+                      }
+
+                      for ( i = 0; i < 2; i++) {
+                        var id = this.brackets[a].owner + this.brackets[a].name + i + '2';
+                        var value = document.getElementById(id).value;
+                        if (this.round5[i] == '' && eliminatedTeams.indexOf(value) < 0) {
+                          document.getElementById(id).style.backgroundColor = '#3498db';
+                        } else if (this.round5[i] === value) {
+                          document.getElementById(id).style.backgroundColor = '#2ecc71';
+                          document.getElementById(id).style.borderColor = '#27ae60';
+                        } else {
+                          document.getElementById(id).style.backgroundColor = '#e74c3c';
+                          document.getElementById(id).style.borderColor = '#c0392b';
+                        }
+
+                        if (this.round5[i] !== '' && this.round5[i] !== value && eliminatedTeams.indexOf(value) < 0) {
+                          eliminatedTeams.push(value);
+                        }
+                      }
+
+                      for ( i = 0; i < 1; i++) {
+                        var id = this.brackets[a].owner + this.brackets[a].name + i + '1';
+                        var value = document.getElementById(id).value;
+                        if (this.round6[i] == '' && eliminatedTeams.indexOf(value) < 0) {
+                          document.getElementById(id).style.backgroundColor = '#3498db';
+                        } else if (this.round6[i] === value) {
+                          document.getElementById(id).style.backgroundColor = '#2ecc71';
+                          document.getElementById(id).style.borderColor = '#27ae60';
+                        } else {
+                          document.getElementById(id).style.backgroundColor = '#e74c3c';
+                          document.getElementById(id).style.borderColor = '#c0392b';
+                        }
+
+                        if (this.round6[i] !== '' && this.round6[i] !== value && eliminatedTeams.indexOf(value) < 0) {
+                          eliminatedTeams.push(value);
+                        }
                       }
                     }
-
-                    for ( i = 0; i < 16; i++) {
-                      var id = this.brackets[a].owner + this.brackets[a].name + i + '16';
-                      var value = document.getElementById(id).value;
-                      if (this.round2[i] == '' && eliminatedTeams.indexOf(value) < 0) {
-                        document.getElementById(id).style.backgroundColor = '#3498db';
-                      } else if (this.round2[i] === value) {
-                        document.getElementById(id).style.backgroundColor = '#2ecc71';
-                        document.getElementById(id).style.borderColor = '#27ae60';
-                      } else {
-                        document.getElementById(id).style.backgroundColor = '#e74c3c';
-                        document.getElementById(id).style.borderColor = '#c0392b';
-                      }
-
-                      if (this.round2[i] !== '' && this.round2[i] !== value && eliminatedTeams.indexOf(value) < 0) {
-                          eliminatedTeams.push(value);
-                      }
-                    }
-
-                    for ( i = 0; i < 8; i++) {
-                      var id = this.brackets[a].owner + this.brackets[a].name + i + '8';
-                      var value = document.getElementById(id).value;
-                      if (this.round3[i] == '' && eliminatedTeams.indexOf(value) < 0) {
-                        document.getElementById(id).style.backgroundColor = '#3498db';
-                      } else if (this.round3[i] === value) {
-                        document.getElementById(id).style.backgroundColor = '#2ecc71';
-                        document.getElementById(id).style.borderColor = '#27ae60';
-                      } else {
-                        document.getElementById(id).style.backgroundColor = '#e74c3c';
-                        document.getElementById(id).style.borderColor = '#c0392b';
-                      }
-
-                      if (this.round3[i] !== '' && this.round3[i] !== value && eliminatedTeams.indexOf(value) < 0) {
-                          eliminatedTeams.push(value);
-                      }
-                    }
-
-                    for ( i = 0; i < 4; i++) {
-                      var id = this.brackets[a].owner + this.brackets[a].name + i + '4';
-                      var value = document.getElementById(id).value;
-                      if (this.round4[i] == '' && eliminatedTeams.indexOf(value) < 0) {
-                        document.getElementById(id).style.backgroundColor = '#3498db';
-                      } else if (this.round4[i] === value) {
-                        document.getElementById(id).style.backgroundColor = '#2ecc71';
-                        document.getElementById(id).style.borderColor = '#27ae60';
-                      } else {
-                        document.getElementById(id).style.backgroundColor = '#e74c3c';
-                        document.getElementById(id).style.borderColor = '#c0392b';
-                      }
-
-                      if (this.round4[i] !== '' && this.round4[i] !== value && eliminatedTeams.indexOf(value) < 0) {
-                          eliminatedTeams.push(value);
-                      }
-                    }
-
-                    for ( i = 0; i < 2; i++) {
-                      var id = this.brackets[a].owner + this.brackets[a].name + i + '2';
-                      var value = document.getElementById(id).value;
-                      if (this.round5[i] == '' && eliminatedTeams.indexOf(value) < 0) {
-                        document.getElementById(id).style.backgroundColor = '#3498db';
-                      } else if (this.round5[i] === value) {
-                        document.getElementById(id).style.backgroundColor = '#2ecc71';
-                        document.getElementById(id).style.borderColor = '#27ae60';
-                      } else {
-                        document.getElementById(id).style.backgroundColor = '#e74c3c';
-                        document.getElementById(id).style.borderColor = '#c0392b';
-                      }
-
-                      if (this.round5[i] !== '' && this.round5[i] !== value && eliminatedTeams.indexOf(value) < 0) {
-                          eliminatedTeams.push(value);
-                      }
-                    }
-
-                    for ( i = 0; i < 1; i++) {
-                      var id = this.brackets[a].owner + this.brackets[a].name + i + '1';
-                      var value = document.getElementById(id).value;
-                      if (this.round6[i] == '' && eliminatedTeams.indexOf(value) < 0) {
-                        document.getElementById(id).style.backgroundColor = '#3498db';
-                      } else if (this.round6[i] === value) {
-                        document.getElementById(id).style.backgroundColor = '#2ecc71';
-                        document.getElementById(id).style.borderColor = '#27ae60';
-                      } else {
-                        document.getElementById(id).style.backgroundColor = '#e74c3c';
-                        document.getElementById(id).style.borderColor = '#c0392b';
-                      }
-
-                      if (this.round6[i] !== '' && this.round6[i] !== value && eliminatedTeams.indexOf(value) < 0) {
-                          eliminatedTeams.push(value);
-                      }
-                    }
-                  }
-                });
-              });
-            });
-          });
-        });
-      });
+                  });
+});
+});
+});
+});
+});
+      }
     });
   }
 
